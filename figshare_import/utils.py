20a85eb8f52f153d812d093557470556cc4bc2a4
Ian Nesbitt <ian.nesbitt@gmail.com>
2024-08-14 11:53:33 -0700

    modifying the location extraction function

--- figshare_import/utils.py
+++ figshare_import/utils.py
@@ -94,39 +94,51 @@ def get_lat_lon(desc: str):
     Parse latitude and longitude from description and convert to decimal degrees.
     Return all pairs found as a list of LatLon3Tuple objects.
     """
+    L = getLogger(__name__)
     patterns = [
-        r'([+-]?\d+(\.\d+)?)°?\s*,?\s*([+-]?\d+(\.\d+)?)°?',  # 8.994410°, - 79.543000°
-        r'([+-]?\d+(\.\d+)?)°?\s*([NS]),?\s*([+-]?\d+(\.\d+)?)°?\s*([EW])',  # 8.910718°N, -79.528919°
-        r'(\d+)°\s*(\d+(\.\d+)?)\'?\s*([NS]),?\s*(\d+)°\s*(\d+(\.\d+)?)\'?\s*([EW])',  # 7° 38.422'N, 81° 42.079'W
-        r'(\d+)°\s*(\d+)\'\s*(\d+(\.\d+)?)\"?\s*([NS]),?\s*(\d+)°\s*(\d+)\'\s*(\d+(\.\d+)?)\"?\s*([EW])',  # 9°9'42.36"N, 79°50'15.67"W
-        r'(\d+)°\s*(\d+)′\s*([NS])\s*latitude,\s*(\d+)°\s*(\d+)′\s*([EW])\s*longitude',  # 9°41′ S latitude, 76°24′ W longitude
-        r'(\d+)°\s*(\d+(\.\d+)?)\'?\s*([NS])\s+(\d+)°\s*(\d+(\.\d+)?)\'?\s*([EW])'  # 8° 38.743'N    79° 2.887'W
+        r'\b([+-]?\d+(\.\d+)?)°?\s*,\s*([+-]?\d+(\.\d+)?)°?\b',  # 8.994410°, - 79.543000°
+        r'\b([+-]?\d+(\.\d+)?)°?\s*([NS]),\s*([+-]?\d+(\.\d+)?)°?\s*([EW])\b',  # 8.910718°N, -79.528919°
+        r'\b(\d+)°\s*(\d+(\.\d+)?)\'?\s*([NS]),\s*(\d+)°\s*(\d+(\.\d+)?)\'?\s*([EW])\b',  # 7° 38.422'N, 81° 42.079'W
+        r'\b(\d+)°\s*(\d+)\'\s*(\d+(\.\d+)?)\"?\s*([NS]),\s*(\d+)°\s*(\d+)\'\s*(\d+(\.\d+)?)\"?\s*([EW])\b',  # 9°9'42.36"N, 79°50'15.67"W
+        r'\b(\d+)°\s*(\d+)′\s*([NS])\s*latitude,\s*(\d+)°\s*(\d+)′\s*([EW])\s*longitude\b',  # 9°41′ S latitude, 76°24′ W longitude
+        r'\b(\d+)°\s*(\d+(\.\d+)?)\'?\s*([NS])\s+(\d+)°\s*(\d+(\.\d+)?)\'?\s*([EW])\b'  # 8° 38.743'N    79° 2.887'W
     ]
-    
-    latlon = []
-    
-    for pattern in patterns:
-        matches = re.findall(pattern, desc)
-        for match in matches:
-            if len(match) == 4:
-                # Decimal degrees format
-                lat = float(match[0])
-                lon = float(match[2])
-            elif len(match) == 6:
-                # Decimal degrees with direction
-                lat = float(match[0]) * (-1 if match[2] == 'S' else 1)
-                lon = float(match[3]) * (-1 if match[5] == 'W' else 1)
-            elif len(match) == 8:
-                # Degrees and decimal minutes with direction
-                lat = dms_to_decimal(match[0], match[1], 0, match[3])
-                lon = dms_to_decimal(match[4], match[5], 0, match[7])
-            elif len(match) == 10:
-                # Degrees, minutes, and seconds with direction
-                lat = dms_to_decimal(match[0], match[1], match[2], match[4])
-                lon = dms_to_decimal(match[5], match[6], match[7], match[9])
-            latlon.append(LatLon3Tuple(lat, lon, 0))
-    return latlon
 
+    latlon = []
+
+    if '°' in desc:
+        for pattern in patterns:
+            matches = re.findall(pattern, desc)
+            for match in matches:
+                L.info(f'Match list: {match}')
+                if len(match) == 4:
+                    # Decimal degrees format
+                    lat = float(match[0])
+                    lon = float(match[2])
+                    L.info(f'Found decimal degrees; lat, lon: {lat}, {lon}')
+                    latlon.append(LatLon3Tuple(lat, lon, 0))
+                elif len(match) == 6:
+                    # Decimal degrees with direction
+                    lat = float(match[0]) * (-1 if match[2] == 'S' else 1)
+                    lon = float(match[3]) * (-1 if match[5] == 'W' else 1)
+                    L.info(f'Found directional decimal degrees; lat, lon: {lat}, {lon}')
+                    latlon.append(LatLon3Tuple(lat, lon, 0))
+                elif len(match) == 8:
+                    # Degrees and decimal minutes with direction
+                    lat = dms_to_decimal(match[0], match[1], 0, match[3])
+                    lon = dms_to_decimal(match[4], match[5], 0, match[7])
+                    L.info(f'Found DDM; lat, lon: {lat}, {lon}')
+                    latlon.append(LatLon3Tuple(lat, lon, 0))
+                elif len(match) == 10:
+                    # Degrees, minutes, and seconds with direction
+                    lat = dms_to_decimal(match[0], match[1], match[2], match[4])
+                    lon = dms_to_decimal(match[5], match[6], match[7], match[9])
+                    L.info(f'Found DMS; lat, lon: {lat}, {lon}')
+                    latlon.append(LatLon3Tuple(lat, lon, 0))
+        return latlon
+    else:
+        L.info('No lat/lon pairs found in description.')
+        return None
 
 def pathify(title: str):
     """
