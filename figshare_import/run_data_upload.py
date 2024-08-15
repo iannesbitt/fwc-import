9cc6585068a5db06cc80a6b75592f4609cdfee2d
Ian Nesbitt <ian.nesbitt@gmail.com>
2024-08-15 05:20:19 -0700

    many changes constituting a working version of the staging script

--- figshare_import/run_data_upload.py
+++ figshare_import/run_data_upload.py
@@ -4,14 +4,15 @@ import datetime
 from pathlib import Path
 
 from d1_client.mnclient_2_0 import *
-from d1_common.types import dataoneTypes
+from d1_common.types import dataoneTypes, exceptions
 from d1_common.resource_map import createSimpleResourceMap
 from d1_common.resource_map import ResourceMap
 
 from logging import getLogger
+from copy import deepcopy
 
 from .defs import fmts, CN_URL, DATA_ROOT, WORK_LOC
-from .utils import get_article_list, load_uploads, save_uploads, write_article, get_token, get_config, get_doipath
+from .utils import get_article_list, load_uploads, save_uploads, write_article, get_token, get_config, get_doipath, generate_access_policy
 from .conv import figshare_to_eml
 
 rpt_txt = """
@@ -52,7 +53,7 @@ def generate_sys_meta(pid: str, sid: str, format_id: str, size: int, md5, now, o
     sys_meta.checksum.algorithm = 'MD5'
     sys_meta.dateUploaded = now
     sys_meta.dateSysMetadataModified = now
-    sys_meta.accessPolicy = generate_public_access_policy()
+    sys_meta.accessPolicy = generate_access_policy()
     return sys_meta
 
 
@@ -83,20 +84,44 @@ def generate_system_metadata(pid: str, sid: str, format_id: str, science_object:
     return sys_meta, md5, size
 
 
-def generate_public_access_policy():
+def sysmeta_obsolete_updates(client: MemberNodeClient_2_0, old_pid: str, new_pid: str):
     """
-    Creates the access policy for the object. Note that the permission is set to 'read'.
-    
-    :return: The access policy.
-    :rtype: dataoneTypes.accessPolicy
+    Update the system metadata of the old object to include the obsoletedBy field.
+
+    :param client: The Member Node client.
+    :type client: MemberNodeClient_2_0
+    :param old_pid: The PID of the old object.
+    :type old_pid: str
+    :param new_pid: The PID of the new object.
+    :type new_pid: str
     """
-    accessPolicy = dataoneTypes.accessPolicy()
-    accessRule = dataoneTypes.AccessRule()
-    accessRule.subject.append(d1_common.const.SUBJECT_PUBLIC)
-    permission = dataoneTypes.Permission('read')
-    accessRule.permission.append(permission)
-    accessPolicy.append(accessRule)
-    return accessPolicy
+    L = getLogger(__name__)
+    # Retrieve old sysmeta
+    old_system_metadata = client.getSystemMetadata(old_pid)
+    if not old_system_metadata:
+        L.error(f'Failed to retrieve system metadata for old PID: {old_pid}')
+        return None
+    new_system_metadata = client.getSystemMetadata(new_pid)
+    if not new_system_metadata:
+        L.error(f'Failed to retrieve system metadata for new PID: {new_pid}')
+        return None
+    # Add obsoletedBy pid to old sysmeta
+    old_system_metadata.obsoletedBy = new_pid
+    new_system_metadata.obsoletes = old_pid
+    try:
+        # Update old sysmeta object
+        client.updateSystemMetadata(old_pid, old_system_metadata)
+        L.info(f'Successfully updated system metadata for old PID: {old_pid} with obsoletedBy: {new_pid}')
+    except Exception as e:
+        L.error(f'Failed to update system metadata for old PID: {old_pid}: {repr(e)}')
+        return None
+    try:
+        # Update new sysmeta object
+        client.updateSystemMetadata(new_pid, new_system_metadata)
+        L.info(f'Successfully updated system metadata for new PID: {new_pid} with obsoletes: {old_pid}')
+    except Exception as e:
+        L.error(f'Failed to update system metadata for new PID: {new_pid}: {repr(e)}')
+        return None
 
 
 def get_format(fmt: Path):
@@ -246,7 +271,7 @@ def upload_eml(orcid: str, doi: str, eml: str, client: MemberNodeClient_2_0):
         return None
 
 
-def generate_resource_map(doi: str, eml_pid: str, data_pids: list):
+def generate_resource_map(eml_pid: str, data_pids: list):
     """
     Generate the resource map XML for the given DOI, EML PID, and data PIDs.
 
@@ -258,9 +283,9 @@ def generate_resource_map(doi: str, eml_pid: str, data_pids: list):
     """
     L = getLogger(__name__)
     resource_map: ResourceMap = createSimpleResourceMap(
-        ore_pid=f"urn:uuid:{str(uuid.uuid4())}",
+        ore_pid=f"resource_map_urn:uuid:{str(uuid.uuid4())}",
         scimeta_pid=eml_pid,
-        scidata_pid_list=data_pids,
+        sciobj_pid_list=data_pids,
     )
     L.debug(f"Generated resource map:\n{resource_map}")
     return resource_map
@@ -283,7 +308,7 @@ def upload_resource_map(doi: str, resource_map: ResourceMap, client: MemberNodeC
     resource_map_bytes = resource_map.serialize(format="xml")
     resource_map_sm, resource_map_md5, resource_map_size = generate_system_metadata(pid=resource_map_pid,
                                                                                     sid=doi,
-                                                                                    format_id="http://www.openarchives.org/ore/terms/ResourceMap",
+                                                                                    format_id="http://www.openarchives.org/ore/terms",
                                                                                     science_object=resource_map_bytes,
                                                                                     orcid=orcid)
     resource_map_dmd = client.create(resource_map_pid, resource_map_bytes, resource_map_sm)
@@ -340,6 +365,7 @@ def upload_manager(articles: list, orcid: str, client: MemberNodeClient_2_0, nod
     succ_list = []
     err_list = []
     uploads_loc = Path(WORK_LOC / f'{node}.json')
+    old_eml_pid, old_resource_map_pid = None, None
     try:
         uploads = load_uploads(uploads_loc)
     except FileNotFoundError:
@@ -353,29 +379,46 @@ def upload_manager(articles: list, orcid: str, client: MemberNodeClient_2_0, nod
             L.info(f'({i}/{n}) Working on {doi}')
             af = write_article(article=article, doi=doi, title='original_metadata', fmt='json')
             files = article.get('files')
-            files.append({'name': f'original_metadata.json', 'computed_md5': hashlib.md5(af.read_bytes()).hexdigest()})
+            files.append({
+                'name': f'original_metadata.json',
+                'computed_md5': hashlib.md5(af.read_bytes()).hexdigest(),
+                'mimetype': 'application/json',
+            })
+            ulist = deepcopy(files)
             if not (uploads.get(doi)):
                 uploads[doi] = {}
             else:
                 prev_upls = 0
                 for uf in uploads.get(doi):
-                    L.debug(f'uploaded file: {uploads[doi][uf]}')
-                    fn = 0
+                    L.debug(f'Already uploaded file: {uploads[doi][uf]}')
                     for f in files:
-                        L.debug(f'known file: {files[fn]}')
+                        if f.get('computed_md5') in uploads[doi]:
+                            f['d1_url'] = uploads[doi][f.get('computed_md5')]['url']
+                            f['pid'] = uploads[doi][f.get('computed_md5')]['identifier']
+                        else:
+                            files.append({
+                                'pid': uploads[doi][uf]['identifier'],
+                                'name': uploads[doi][uf]['filename'],
+                                'computed_md5': uf,
+                                'd1_url': uploads[doi][uf]['url'],
+                                'size': uploads[doi][uf]['size'],
+                                'mimetype': uploads[doi][uf]['formatId'],
+                            })
+                    fn = 0
+                    for f in ulist:
                         if uploads[doi][uf]['filename'] == f['name']:
                             prev_upls += 1
-                            del files[fn]
+                            del ulist[fn]
                         fn += 1
                 L.info(f'Found {prev_upls} files that were already uploaded associated with {doi}')
             try:
-                if len(files) > 0:
-                    sm_dict = upload_files(orcid, doi, files, client)
+                if len(ulist) > 0:
+                    sm_dict = upload_files(orcid, doi, ulist, client)
                     L.info(f'{doi} done. Uploaded {len(sm_dict)} files.')
                     for fi in sm_dict:
                         uploads[doi][fi] = sm_dict[fi]
                         for f in files:
-                            if f['name'] == sm_dict[fi]['filename'] and f['computed_md5'] == sm_dict[fi]['md5']:
+                            if (f['name'] == sm_dict[fi]['filename']) and (f['computed_md5'] == fi):
                                 f['d1_url'] = sm_dict[fi]['url']
                     save_uploads(uploads, fp=uploads_loc)
                 else:
@@ -383,9 +426,16 @@ def upload_manager(articles: list, orcid: str, client: MemberNodeClient_2_0, nod
                 # Convert the article to EML
                 eml_string = figshare_to_eml(article)
                 # Write the EML to file
-                write_article(article=article, doi=doi, title=title, fmt='xml')
+                write_article(article=eml_string, doi=doi, title=title, fmt='xml')
                 # Upload the EML to the Member Node
+                if uploads[doi].get('eml'):
+                    old_eml_pid = uploads[doi]['eml']['identifier']
+                    L.info(f'{doi} Found previous EML: {old_eml_pid}')
                 eml_pid, eml_md5, eml_size = upload_eml(orcid, doi, eml_string, client)
+                if old_eml_pid:
+                    L.info(f'{doi} Adding obsoletedBy to old EML sysmeta object: {uploads[doi]["eml"]["identifier"]}')
+                    # Update old sysmeta with obsoletedBy
+                    sysmeta_obsolete_updates(client, old_eml_pid, eml_pid)
                 if eml_pid:
                     uploads[doi]['eml'] = {
                         'filename': "eml_2_2_0.xml",
@@ -398,13 +448,29 @@ def upload_manager(articles: list, orcid: str, client: MemberNodeClient_2_0, nod
                     }
                     save_uploads(uploads, fp=uploads_loc)
                     # Generate the DataONE resource map
-                    pid_list = []
-                    for file_info in files:
-                        pid_list.append(file_info['identifier'])
-                    pid_list.append(eml_pid)
-                    resource_map = generate_resource_map(doi=doi, eml_pid=eml_pid, data_pids=pid_list)
+                    pid_list = [eml_pid]
+                    for f in files:
+                        if f.get('pid'):
+                            pid_list.append(f['pid'])
+                        else:
+                            if f.get('computed_md5') in sm_dict:
+                                pid_list.append(sm_dict[f['computed_md5']]['identifier'])
+                            else:
+                                L.error(f'{doi} No PID found for {f["name"]}')
+                    resource_map = generate_resource_map(eml_pid=eml_pid, data_pids=pid_list)
+                    if uploads[doi].get('resource_map'):
+                        old_resource_map_pid = uploads[doi]['resource_map']['identifier']
+                        L.info(f'{doi} Found previous resource map: {old_resource_map_pid}')
                     # Upload the resource map to the Member Node
-                    resource_map_pid, resource_map_md5, resource_map_size = upload_resource_map(orcid, doi, resource_map, client)
+                    resource_map_pid, resource_map_md5, resource_map_size = upload_resource_map(
+                        doi=doi,
+                        resource_map=resource_map,
+                        client=client,
+                        orcid=orcid,
+                    )
+                    if old_resource_map_pid:
+                        L.info(f'{doi} Adding obsoletedBy to old resource map sysmeta object: {old_resource_map_pid}')
+                        sysmeta_obsolete_updates(client, old_pid=old_resource_map_pid, new_pid=resource_map.getResourceMapPid())
                     # Put the resource map info in the uploads dictionary
                     if resource_map_pid:
                         uploads[doi]['resource_map'] = {
@@ -413,15 +479,15 @@ def upload_manager(articles: list, orcid: str, client: MemberNodeClient_2_0, nod
                             'doi': doi,
                             'identifier': resource_map_pid,
                             'md5': resource_map_md5,
-                            'formatId': "http://www.openarchives.org/ore/terms/ResourceMap",
+                            'formatId': "http://www.openarchives.org/ore/terms",
                             'url': f"{CN_URL}{sep}v2/resolve/{resource_map_pid}",
                         }
                         save_uploads(uploads, fp=uploads_loc)
                         L.info(f'{doi} Resource map uploaded successfully: {resource_map_pid}')
                     else:
-                        L.error(f'{doi} Resource map upload failed')
+                        raise exceptions.DataONEException(f'{doi} Resource map upload failed')
                 else:
-                    L.error(f'{doi} EML upload failed')
+                    raise exceptions.DataONEException(f'{doi} EML upload failed')
                 succ_list.append(doi)
             except Exception as e:
                 er += 1
@@ -442,7 +508,11 @@ def main():
     L = getLogger(__name__)
     # Set config items
     auth_token = get_token()
-    orcid, node, mn_url, metadata_json = get_config()
+    config = get_config()
+    orcid = config.get('rightsholder_orcid')
+    node = config.get('nodeid')
+    mn_url = config.get('mnurl')
+    metadata_json = config.get('metadata_json')
     L.info(f'Rightsholder ORCiD {orcid}')
     L.info(f'Using {node} at {mn_url}')
     L.info(f'Root path: {DATA_ROOT}')
